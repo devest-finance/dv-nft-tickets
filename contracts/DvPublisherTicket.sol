53238a4ad17c70464bc8a5525c5d0124ab21c126
Noel Koljanin <82367504+nkoljanin@users.noreply.github.com>
2024-11-11 10:51:26 +0100

    Initial commit

---
+++ contracts/DvPublisherTicket.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/utils/Context.sol"; // Importing Context for basic execution context utilities (e.g., msg.sender).
+import "./DvTicket.sol"; // Importing the DvTicket contract as a base for the ticketing system.
+
+/**
+ * @title DvVisitorTicket
+ * @dev Extends the DvTicket contract to implement a specific use case of NFTs (Non-Fungible Tokens),
+ * representing visitor tickets with unique identifiers and metadata.
+ * 
+ * Each ticket (NFT) is associated with a root ID and URI, indicating a common origin or category
+ * for the tickets. This can be used to group tickets by event, location, or other criteria.
+ */
+contract DvPublisherTicket is DvTicket {
+    // The root ID for a group of tickets, used to associate individual tickets with a common origin.
+    uint256 private _rootId;
+    
+    // The root URI for the group of tickets, providing base metadata or information about the common origin.
+    string private _rootUri;
+
+    /**
+     * @dev Constructor to create a DvPublisherTicket instance.
+     * @param name The name of the ticket category.
+     * @param symbol The symbol representing the ticket category.
+     * @param __rootId The root ID shared by all tickets in this category.
+     * @param __rootUri The root URI containing base information or metadata for the tickets.
+     * @param owner The owner of the ticket category, typically the contract deployer.
+     * @param factory The factory or contract that is responsible for managing this category.
+     */
+    constructor(string memory name, string memory symbol, uint256 __rootId, string memory __rootUri, address owner, address factory) 
+    DvTicket(name, symbol, owner, factory) {
+        _rootId = __rootId;
+        _rootUri = __rootUri;
+    }
+
+    /**
+     * @dev Retrieves the root ID associated with this ticket collection.
+     * @return The root ID as a uint256.
+     */
+    function rootId() public view returns (uint256) {
+        return _rootId;
+    }
+
+    /**
+     * @dev Retrieves the root URI containing base metadata or information for the ticket collection.
+     * @return The root URI as a string.
+     */
+    function rootUri() public view returns (string memory) {
+        return _rootUri;
+    }
+
+    /**
+     * @notice Issues a new Visitor ticket (NFT) to a specified address.
+     * @dev Mints a new ticket with a unique ID and associates it with a specific URI. Can only be called by the collection owner.
+     * @param to The address to which the new ticket will be issued.
+     * @param tokenId The unique identifier for the new ticket.
+     * @param uri The URI containing metadata for the new ticket.
+     */
+    function issueNFT(address to, uint256 tokenId, string memory uri)
+        public payable 
+        onlyOwner // Restricting this function to the collection owner.
+    {
+        _mint(to, tokenId); // Minting a new ticket.
+        _setTokenURI(tokenId, uri); // Setting the URI for the new ticket.
+    }
+}
---
+++ contracts/DvVisitorTicketFactory.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+// Importing the DvFactory contract as the base for this factory contract.
+import "@devest/contracts/DvFactory.sol";
+// Importing the DvVisitorTicket contract which defines the tickets to be managed by this factory.
+import "./DvPublisherTicket.sol";
+
+/**
+ * @title DvVisitorTicketFactory
+ * @dev Contract to create and manage DvVisitorTicket. Inherits from DvFactory for common factory functionality.
+ */
+contract DvPublisherTicketFactory is DvFactory {
+
+    /**
+     * @dev Detaches a DvVisitorTicket ticket from this factory's control, making it independent.
+     * Can only be called by the owner of the factory.
+     * @param _ticketAddress Address of the DvVisitorTicket ticket to be detached.
+     */
+    function detach(address payable _ticketAddress) external payable onlyOwner {
+        DvPublisherTicket ticket = DvPublisherTicket(_ticketAddress);
+        ticket.detach();
+    }
+
+    /**
+     * @dev Creates a new DvVisitorTicket ticket with specified attributes and registers it with the factory.
+     * Charges an issue fee if applicable.
+     * @param name Name of the new ticket.
+     * @param symbol Symbol of the new ticket.
+     * @param rootId An identifier for the root ticket, if applicable.
+     * @param rootUri URI for the root metadata.
+     * @return The address of the newly created DvVisitorTicket ticket.
+     */
+    function issue(string memory name, string memory symbol, uint256 rootId, string memory rootUri) public payable isActive returns (address) {
+        // Verifies the transaction includes enough ETH to cover the issue fee.
+        require(msg.value >= _issueFee, "Please provide enough fee");
+        
+        // If there's an issue fee defined, transfers it to the fee recipient.
+        if (_issueFee > 0)
+            payable(_feeRecipient).transfer(_issueFee);
+
+        // Creates a new DvVisitorTicket ticket with the provided parameters, setting the issuer (msg.sender) and this factory as its manager.
+        DvVisitorTicket ticket = new DvVisitorTicket(name, symbol, rootId, rootUri, _msgSender(), address(this));
+
+        // Emits an event indicating a new ticket has been deployed.
+        emit deployed(_msgSender(), address(ticket));
+
+        // Returns the address of the newly created ticket.
+        return address(ticket);
+    }
+}
